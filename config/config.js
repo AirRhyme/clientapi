20aa300aebcdb61c8560e0a7b931d0d972f2555d
AirRhyme <84876147+AirRhyme@users.noreply.github.com>
2022-09-01 19:12:51 -0400

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,15 @@
+//[major].[minor].[patch]-[release/beta]
+module.exports = {
+    //DONT CHANGE ME
+    version: '1.0.0-Beta',
+    
+    //Change anything below this line
+    client_secret: 'AWbis0212ConnectError',
+    port: 8080,
+    
+    clientSettings: {
+        isWhitelisted: false,
+        latestVersion: "1.1"
+        /*Add anything here you want to be included in /api/globalSettings*/
+    }
+}
